021db9aad151dba847c64ada8249fd6786ed108d
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-04-28 22:26:17 +0200

    Fixes
    Former-commit-id: f3573cd8ef7b3b59232384e0ee5f32fdaeb59a34

--- utils/eval.py
+++ utils/eval.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score, average_precision_score
-
+from utils.chords import chords, chromatic_scale
 
 def itemwise_auc_ap(y, pred):
     """ Annotation : item-wise(row wise) calculation """
@@ -39,13 +39,10 @@ def average_precision(y_targets, y_preds):
     for by, bp in zip(y_targets, y_preds):
         ap.append(average_precision_score(by, bp))
         # acc = accuracy_score(y.argmax(1).detach().cpu().numpy(), output.argmax(1).detach().cpu().numpy())
-
     return np.array(ap)
 
 
-def eval_all(
-    args, loader, simclr_model, model, writer, n_tracks=None
-):
+def eval_all(args, loader, simclr_model, model, writer, n_tracks=None):
     model.eval()
     predicted_classes = torch.zeros(args.n_classes).to(args.device)
     pred_array = []
@@ -113,3 +110,87 @@ def eval_all(
     )
     model.train()
     return auc, ap
+
+
+def is_fifth(tonic_p, mode_p, tonic_l, mode_l):
+    """
+    Calculate if the tonic of the prediction is the fifth of the target (matching modes)
+    """
+    return (tonic_p == fifths[tonic_l]) and (mode_p == mode_l)
+
+
+def is_relative(tonic_p, mode_p, tonic_l, mode_l):
+    """
+    Calculate if modes differ and either:
+    a) the predicted mode is minor and the predicted tonic is 3 semitones below the target, or 
+    b) the predicted mode is major and the predicted tonic is 3 semitones above the target
+    """
+
+    if mode_p != mode_l:
+        return False
+
+    chrom_p = chromatic_scale[tonic_p]
+    chrom_l = chromatic_scale[tonic_l]
+    if mode_p == "min":
+        if chrom_p == chrom_l - 3:
+            return True
+    if mode_p == "maj":
+        if chrom_p == chrom_l + 3:
+            return True
+
+    return False
+
+
+def is_parallel(tonic_p, mode_p, tonic_l, mode_l):
+    """
+    Calculate if modes differ but the predicted tonic matches the target.
+    """
+    return (mode_p != mode_l) and (tonic_p == tonic_l)
+
+
+def evaluate_key_mirex(predictions, labels):
+    """
+    MIREX weighted key evaluation
+    """
+
+    metrics = {}
+    metrics["correct"] = (predictions == labels).sum().item()  # correct classifications
+
+    # if the tonic of the prediction is the fifth of the target (or vice versa), and modes correspond.
+    metrics["fifth"] = 0  #
+    metrics["relative"] = 0
+    metrics["parallel"] = 0
+    metrics["other"] = 0
+
+    key_names = list(chords.keys())
+    for p, l in zip(predictions, labels):
+        key_p, key_l = p.item(), l.item()
+
+        key_p_name = key_names[key_p]
+        key_l_name = key_names[key_l]
+        tonic_p, mode_p = key_p_name.split(":")
+        tonic_l, mode_l = key_l_name.split(":")
+
+        if is_fifth(tonic_p, mode_p, tonic_l, mode_l):
+            metrics["fifth"] += 1
+            # print('fifth', key_p, tonic_p, mode_p, key_l, tonic_l, mode_l)
+        elif is_relative(tonic_p, mode_p, tonic_l, mode_l):
+            metrics["relative"] += 1
+            # print('rel', key_p, tonic_p, mode_p, key_l, tonic_l, mode_l)
+        elif is_parallel(tonic_p, mode_p, tonic_l, mode_l):
+            metrics["parallel"] += 1
+            # print('parallel', key_p, tonic_p, mode_p, key_l, tonic_l, mode_l)
+        else:
+            metrics["other"] += 1
+
+    weighted = (
+        metrics["correct"]
+        + 0.5 * metrics["fifth"]
+        + 0.3 * metrics["relative"]
+        + 0.2 * metrics["parallel"]
+    )
+
+    metrics["other"] = labels.size(0) - metrics["other"]
+    weighted = weighted / labels.size(0)
+    metrics = {k: v / labels.size(0) for k, v in metrics.items()}
+    return weighted, metrics
