55d9ad03bd9f64be02ab243f9790973c5f264a27
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-05-01 08:21:16 -0400

    Cleaning
    Former-commit-id: 7a6d07434e890f8f07bd6a55f689e770241cfb61

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-# activate pip / conda environment first
-
-# Train SimCLR model
-python main.py
-
-# Train linear model and run test
-python -m testing.logistic_regression \
-    with \
-    model_path=./logs/0
