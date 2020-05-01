de22bdc2931c53e129531de5ba50073f2c20a4b7
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-05-01 08:40:42 -0400

    Added audio to tb
    Former-commit-id: a6e49414b27cd22524657b52a9ffa20ad74112ee

--- utils/audio.py
+++ utils/audio.py
@@ -1,4 +1,48 @@
 import torchaudio
+import numpy as np
 
 def tensor_to_audio(fn, t, sr):
     torchaudio.save(fn, t.cpu(), sample_rate=sr)
+
+
+def write_audio_tb(args, train_loader, test_loader, writer, num_audio=5):
+    idx = np.random.choice(len(train_loader.dataset), num_audio)
+    for idx, r_idx in enumerate(idx):
+        ((x_i, x_j), y, track_id) = train_loader.dataset[r_idx]
+        x_i = train_loader.dataset.denormalise_audio(x_i)
+        x_j = train_loader.dataset.denormalise_audio(x_j)
+        writer.add_audio(
+            f"audio/train-{idx}-{track_id}-xi",
+            x_i,
+            global_step=None,
+            sample_rate=args.sample_rate,
+            walltime=None,
+        )
+        writer.add_audio(
+            f"audio/train-{idx}-{track_id}-xj",
+            x_j,
+            global_step=None,
+            sample_rate=args.sample_rate,
+            walltime=None,
+        )
+
+    idx = np.random.choice(len(test_loader.dataset), num_audio)
+    for idx, r_idx in enumerate(idx):
+        ((x_i, x_j), y, track_id) = test_loader.dataset[r_idx]
+        x_i = train_loader.dataset.denormalise_audio(x_i)
+        x_j = train_loader.dataset.denormalise_audio(x_j)
+        writer.add_audio(
+            f"audio/test-{idx}-{track_id}-xi",
+            x_i,
+            global_step=None,
+            sample_rate=args.sample_rate,
+            walltime=None,
+        )
+        writer.add_audio(
+            f"audio/test-{idx}-{track_id}-xj",
+            x_j,
+            global_step=None,
+            sample_rate=args.sample_rate,
+            walltime=None,
+        )
+
