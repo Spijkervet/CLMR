d459f7ee949b52fa64bd255d6b28efe92831aad4
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-05-03 17:19:53 -0400

    Kaiming init
    Former-commit-id: 95a748004d45a3a63b991a903b449095352a33ac

--- modules/model.py
+++ modules/model.py
@@ -1,10 +1,16 @@
 import torch.nn as nn
 import numpy as np
 
-class Model(nn.Module):
 
+class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
+
+    def initialize(self, m):
+        if isinstance(m, (nn.Conv1d)):
+            nn.init.kaiming_uniform_(
+                m.weight, mode="fan_in", nonlinearity="relu"
+            )
 
     def summary(self):
         model_parameters = filter(lambda p: p.requires_grad, self.parameters())
