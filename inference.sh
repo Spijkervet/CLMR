4552c6b1a2359672778456ac9c4f9d15100a2411
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-11 00:05:36 +0200

    Removed redundant files
    Former-commit-id: 45b10d7ca0289dc50f7cca43ec913cec74caa775
    Former-commit-id: b5fcb7d10e54d32d4019f0c23abc114191ebeb6d

--- inference.sh
+++
@@ -1,8 +0,0 @@
-python inference.py \
-    with \
-    audio_url="https://www.youtube.com/watch?v=ftjEcrrf7r0" \
-    model_path=/storage/jspijkervet/logs_backup_ws7/clmr/2/ \
-    epoch_num=1490 \
-    finetune_model_path=/storage/jspijkervet/logs_backup_ws7/clmr/4/ \
-    finetune_epoch_num=50 \
-    -u # do not log
