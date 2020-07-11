9d116184e862b4941eb8e9fdb5e5a38293308602
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-11 13:56:48 +0200

    Fixed to mtt
    Former-commit-id: 0025f9ed7e46fa2c10359b35466e2d3be16317a5 [formerly f6f9f883421659d7bef1857468df3b8ee3c973d2] [formerly 139de4764aad6306964003aba4d90fd0a6af88be]
    Former-commit-id: 8c24d9da6eda010847007d96c913c1a78e0fbb66
    Former-commit-id: 85e7947e9d6938f749b8d33234622e768accb5bb

--- data/audio/dataset.py
+++ data/audio/dataset.py
@@ -1,27 +1,51 @@
+import os
+from pathlib import Path
+from collections import defaultdict
 import torch
 import torch.nn as nn
 import torchaudio
 from torch.utils.data import Dataset as TorchDataset
 
+import soundfile as sf
+
 # inherits Dataset from PyTorch
 class Dataset(TorchDataset):
 
-    def __init__(self, sample_rate, audio_length, tracks_list, tracks_list_all, mean, std):
+    def __init__(self, sample_rate, audio_length, tracks_list, num_segments, mean, std):
         self.sample_rate = sample_rate
         self.audio_length = audio_length
         self.tracks_list = tracks_list
-        self.tracks_list_all = tracks_list_all
+        self.num_segments = num_segments 
         self.mean = mean
         self.std = std
 
+    def indexer(self, ids, id2audio_path, id2gt):
+        index = []
+        track_index = defaultdict(list)
+        track_id = 0
+        for clip_id in ids:
+            fp = id2audio_path[clip_id]
+            label = id2gt[clip_id]
+            full_track = "".join(Path(fp).stem.split("-")[:-2])
+            if full_track not in track_index:
+                track_index[full_track] = track_id
+                track_id += 1
+
+            target_fn = f"{track_id}-{clip_id}-{self.sample_rate}.wav"
+            for s in range(self.num_segments): 
+                index.append([track_id, clip_id, s, target_fn, label])
+                track_index[track_id].append([clip_id, s, target_fn, label])
+        return index, track_index
+
     def loader(self, path):
+        # audio, sr = sf.read(path, dtype="float32")
+        # audio = torch.from_numpy(audio).reshape(1, -1)
         audio, sr = torchaudio.load(path, normalization=True)
         return audio, sr
 
     def get_audio(self, fp):
         audio, sr = self.loader(fp)
         max_samples = audio.shape[1]
-
         if sr != self.sample_rate:
             raise Exception("Sample rate is not consistent throughout the dataset")
 
@@ -65,8 +89,8 @@ class Dataset(TorchDataset):
         """
         batch = torch.zeros(batch_size, 1, self.audio_length)
         for idx in range(batch_size):
-            index = self.tracks_dict[track_id][0]
-            _, fp, label, _ = self.tracks_list_all[index]  # from non-dup!!
+            track_id, clip_id, segment, label, fp = self.track_index[track_id][0]
+            # track_id, fp, label, _ = self.index[track_index]  # from non-dup!!
 
             audio = self.get_audio(fp)
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .yaml_config_hook import yaml_config_hook 
 from .audio import tensor_to_audio, write_audio_tb
 from .eval import tagwise_auc_ap, eval_all
-from .misc import args_hparams
 from .web import tsne_to_json
 from .subsample import random_undersample_balanced
+from .argparser import parse_args, args_hparams
