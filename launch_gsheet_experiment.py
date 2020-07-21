5d33b98dc331a7b54d71223879986b4996478e9e
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-21 15:25:40 +0200

    Added gsheet experiment launcher
    Former-commit-id: e44102cd51cea8447f03bf1d7d4016e5011d315b
    Former-commit-id: 4217953686ba9bf5b49f6c54ecd17f88b4c382d1

---
+++ launch_gsheet_experiment.py
@@ -0,0 +1,51 @@
+import csv
+import requests
+import socket
+
+def csv_to_dict(content):
+    reader = csv.reader(content.splitlines(), delimiter=',')
+    header = next(reader)
+    experiments = {}
+    for row in reader:
+        d = {}
+        for idx, r in enumerate(row):
+            d[header[idx]] = r
+
+        idx = d["id"]
+        del d["id"]
+        experiments[idx] = d
+    return experiments
+
+def get_host_experiment(experiments):
+    num_exp = 0
+    exp = None
+    for exec_id, params in experiments.items():
+        if params["host"] == socket.gethostname() and params["status"] == "pending":
+            exp = experiments[exec_id]
+            num_exp += 1
+    assert num_exp < 2, "There are multiple experiments defined for this machine. Set status to 'running' to start a new one on the same machine."
+    return exp
+
+with requests.Session() as s:
+    r = s.get('https://docs.google.com/spreadsheet/ccc?key=1iSKcoSqA7tOKcsuaKgV_Bz57V_2yZ_XXe9nn1-ApCEA&output=csv')
+    assert r.status_code == 200, 'Wrong status code'
+    decoded = r.content.decode("utf-8")
+    experiments = csv_to_dict(decoded)
+    host_experiment = get_host_experiment(experiments)
+    
+    gpu_nr = host_experiment["gpu_nr"]
+    # delete unused keys
+    del host_experiment["gpu_nr"]
+    del host_experiment["status"]
+    del host_experiment["host"]
+    del host_experiment["roc_auc"]
+    del host_experiment["pr_auc"]
+    del host_experiment["roc_auc_mlp"]
+    del host_experiment["pr_auc_mlp"]
+    cmd = "CUDA_VISIBLE_DEVICES={} python main.py ".format(gpu_nr)
+    cmd += " ".join(["--{} {}".format(k, v) for k, v in host_experiment.items()])
+
+    print("Copy/paste the following command to start training:")
+    print()
+    print(cmd)
+    print()
