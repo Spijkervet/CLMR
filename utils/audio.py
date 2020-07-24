6c23052c3a5b923ec0be93aca924b317f9b941d5
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-24 21:08:46 +0200

    Added pitch transform
    Former-commit-id: cda2a92edbb19002d498c4926a4801d99e196a3a
    Former-commit-id: 13195114d6e5e97eda6d64846ad5902ef8db8b78

--- utils/audio.py
+++ utils/audio.py
@@ -15,21 +15,21 @@ def tensor_to_audio(fn, t, sr):
 def write_audio_tb(args, train_loader, test_loader, writer, num_audio=5):
     idx = np.random.choice(len(train_loader.dataset), num_audio)
     for idx, r_idx in enumerate(idx):
-        ((x_i, x_j), y, track_id) = train_loader.dataset[r_idx]
+        ((x_i, x_j), y) = train_loader.dataset[r_idx]
 
         if train_loader.dataset.mean is not None:
             x_i = train_loader.dataset.denormalise_audio(x_i)
             x_j = train_loader.dataset.denormalise_audio(x_j)
 
         writer.add_audio(
-            f"audio/train-{idx}-{track_id}-xi",
+            f"audio/train-{idx}-xi",
             x_i,
             global_step=None,
             sample_rate=args.sample_rate,
             walltime=None,
         )
         writer.add_audio(
-            f"audio/train-{idx}-{track_id}-xj",
+            f"audio/train-{idx}-xj",
             x_j,
             global_step=None,
             sample_rate=args.sample_rate,
@@ -38,20 +38,20 @@ def write_audio_tb(args, train_loader, test_loader, writer, num_audio=5):
 
     idx = np.random.choice(len(test_loader.dataset), num_audio)
     for idx, r_idx in enumerate(idx):
-        ((x_i, x_j), y, track_id) = test_loader.dataset[r_idx]
+        ((x_i, x_j), y) = test_loader.dataset[r_idx]
         if train_loader.dataset.mean is not None:
             x_i = train_loader.dataset.denormalise_audio(x_i)
             x_j = train_loader.dataset.denormalise_audio(x_j)
             
         writer.add_audio(
-            f"audio/test-{idx}-{track_id}-xi",
+            f"audio/test-{idx}-xi",
             x_i,
             global_step=None,
             sample_rate=args.sample_rate,
             walltime=None,
         )
         writer.add_audio(
-            f"audio/test-{idx}-{track_id}-xj",
+            f"audio/test-{idx}-xj",
             x_j,
             global_step=None,
             sample_rate=args.sample_rate,
