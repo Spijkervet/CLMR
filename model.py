17d84757256b1d329a9386da583fafad874450a4
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-08-06 20:02:14 +0200

    Updates
    Former-commit-id: de7428e7bc4e9c13559e1145654ae250129a5c6a
    Former-commit-id: c6932d439491e390a78c11ba7950fa43146095a4

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ def load_optimizer(args, model):
 
     scheduler = None
     if args.optimizer == "Adam":
-        optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)  # TODO: LARS
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)  # TODO: LARS
     elif args.optimizer == "LARS":
         # optimized using LARS with linear learning rate scaling
         # (i.e. LearningRate = 0.3 × BatchSize/256) and weight decay of 10−6.
