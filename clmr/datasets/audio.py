f7a17951f2df325bbbb443ec6e3a2e6b045b7937
Janne Spijkervet <janne.spijkervet@gmail.com>
2021-03-18 16:48:50 +0000

    fixed audio loader

--- clmr/datasets/audio.py
+++ clmr/datasets/audio.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from glob import glob
 from collections import defaultdict
 from typing import Any, Tuple, Optional
@@ -8,9 +9,11 @@ from torch import Tensor
 from torch.utils.data import Dataset
 
 
-def load_audio(audio_path):
-    audio, sr = torchaudio.load(audio_path)
-    return audio
+def preprocess_audio(source, target, sample_rate):
+    p = subprocess.Popen(
+        ["ffmpeg", "-i", source, "-ar", str(sample_rate), target, "-loglevel", "quiet"]
+    )
+    p.wait()
 
 
 class AUDIO(Dataset):
@@ -30,13 +33,15 @@ class AUDIO(Dataset):
         root: str,
     ) -> None:
 
-        self.root = root
         self._path = root
+        self.n_classes = 1
 
-        self.fl = glob(os.path.join(self.root, "*.wav"))
-        self.fl.extend(glob(os.path.join(self.root, "*.mp3")))
-        self.fl.extend(glob(os.path.join(self.root, "**", "*.wav")))
-        self.fl.extend(glob(os.path.join(self.root, "**", "*.mp3")))
+        self.fl = glob(os.path.join(self._path, "*.wav"))
+        self.fl.extend(glob(os.path.join(self._path, "*.mp3")))
+        self.fl.extend(glob(os.path.join(self._path, "*.flac")))
+        self.fl.extend(glob(os.path.join(self._path, "**", "*.wav")))
+        self.fl.extend(glob(os.path.join(self._path, "**", "*.mp3")))
+        self.fl.extend(glob(os.path.join(self._path, "**", "*.flac")))
 
         if len(self.fl) == 0:
             raise RuntimeError(
@@ -44,6 +49,31 @@ class AUDIO(Dataset):
                     self._path
                 )
             )
+
+    def file_path(self, n: int) -> str:
+        fp = self.fl[n]
+        return os.path.join(self._path, fp)
+
+    def target_file_path(self, n: int) -> str:
+        fp = self.file_path(n)
+        file_basename, _ = os.path.splitext(fp)
+        return file_basename + self._ext_audio
+
+    def preprocess(self, n: int, sample_rate: int):
+        fp = self.file_path(n)
+        target_fp = self.target_file_path(n)
+
+        if not os.path.exists(target_fp):
+            preprocess_audio(fp, target_fp, sample_rate)
+
+    def load(self, n):
+        target_fp = self.target_file_path(n)
+        try:
+            audio, sample_rate = torchaudio.load(target_fp)
+        except OSError as e:
+            print("File not found, try running `python preprocess.py` first.\n\n", e)
+            return
+        return audio, sample_rate
 
     def __getitem__(self, n: int) -> Tuple[Tensor, Tensor]:
         """Load the n-th sample from the dataset.
@@ -54,9 +84,8 @@ class AUDIO(Dataset):
         Returns:
             tuple: ``(waveform, label)``
         """
-        audio_path = self.fl[n]
-        audio = load_audio(audio_path)
-        label = None
+        audio, _ = self.load(n)
+        label = []
         return audio, label
 
     def __len__(self) -> int:
