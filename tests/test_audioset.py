7170def0172f86be7b829c978e4add1e285d6611
Janne <janne.spijkervet@gmail.com>
2021-03-18 20:26:17 +0800

    wrapped tests in class

--- tests/test_audioset.py
+++ tests/test_audioset.py
@@ -1,28 +1,33 @@
+import unittest
 import torchaudio
-from torchaudio_augmentations import *
+from torchaudio_augmentations import Compose, RandomApply, RandomResizedCrop, PolarityInversion, Noise, Gain, Delay, PitchShift, Reverb
 from clmr.datasets import AUDIO
 
-sr = 22050
 
 
-def test_audioset():
-    audio_dataset = AUDIO("tests/data/audioset")
-    audio, label = audio_dataset[0]
-    assert audio.shape[0] == 1
-    assert audio.shape[1] == 93680
+class TestAudioSet(unittest.TestCase):
+    sample_rate = 22050
 
-    num_samples = sr
-    transform = Compose(
-        [
-            RandomResizedCrop(n_samples=num_samples),
-            RandomApply([PolarityInversion()], p=0.8),
-            RandomApply([Noise(min_snr=0.3, max_snr=0.5)], p=0.3),
-            RandomApply([Gain()], p=0.2),
-            RandomApply([Delay(sample_rate=sr)], p=0.5),
-            RandomApply([PitchShift(n_samples=num_samples, sample_rate=sr)], p=0.4),
-            RandomApply([Reverb(sample_rate=sr)], p=0.3),
-        ]
-    )
+    def get_audio_transforms(self, num_samples):
+        transform = Compose(
+            [
+                RandomResizedCrop(n_samples=num_samples),
+                RandomApply([PolarityInversion()], p=0.8),
+                RandomApply([Noise(min_snr=0.3, max_snr=0.5)], p=0.3),
+                RandomApply([Gain()], p=0.2),
+                RandomApply([Delay(sample_rate=self.sample_rate)], p=0.5),
+                RandomApply([PitchShift(n_samples=num_samples, sample_rate=self.sample_rate)], p=0.4),
+                RandomApply([Reverb(sample_rate=self.sample_rate)], p=0.3),
+            ]
+        )
+        return transform
 
-    audio = transform(audio)
-    torchaudio.save("augmented_sample.wav", audio, sample_rate=sr)
+    def test_audioset(self):
+        audio_dataset = AUDIO("tests/data/audioset")
+        audio, label = audio_dataset[0]
+        assert audio.shape[0] == 1
+        assert audio.shape[1] == 93680
+
+        transform = self.get_audio_transforms(num_samples=self.sample_rate)
+        audio = transform(audio)
+        torchaudio.save("augmented_sample.wav", audio, sample_rate=self.sample_rate)
--- tests/test_spectogram.py
+++ tests/test_spectogram.py
@@ -1,23 +1,51 @@
+import unittest
 import torchaudio
 import torch.nn as nn
+from torchaudio_augmentations import *
 
 from clmr.datasets import AUDIO
 
-def test_audioset():
-    audio_dataset = AUDIO("tests/data/audioset")
-    audio, label = audio_dataset[0]
 
+class TestAudioSet(unittest.TestCase):
     sample_rate = 22050
-    n_fft = 1024
-    n_mels = 128
-    stype = "magnitude"  # magnitude
-    top_db = None  # f_max
 
-    transform = nn.Sequential(
-        torchaudio.transforms.MelSpectrogram(
-            sample_rate=sample_rate,
-            n_fft=n_fft,
-            n_mels=n_mels,
-        ),
-        torchaudio.transforms.AmplitudeToDB(stype=stype, top_db=top_db),
-    )
+    def get_audio_transforms(self, num_samples):
+        transform = Compose(
+            [
+                RandomResizedCrop(n_samples=num_samples),
+                RandomApply([PolarityInversion()], p=0.8),
+                RandomApply([Noise(min_snr=0.3, max_snr=0.5)], p=0.3),
+                RandomApply([Gain()], p=0.2),
+                RandomApply([Delay(sample_rate=self.sample_rate)], p=0.5),
+                RandomApply([PitchShift(n_samples=num_samples, sample_rate=self.sample_rate)], p=0.4),
+                RandomApply([Reverb(sample_rate=self.sample_rate)], p=0.3),
+            ]
+        )
+        return transform
+
+    def test_audioset(self):
+        audio_dataset = AUDIO("tests/data/audioset")
+        audio, label = audio_dataset[0]
+
+        sample_rate = 22050
+        n_fft = 1024
+        n_mels = 128
+        stype = "magnitude"  # magnitude
+        top_db = None  # f_max
+
+        transform = self.get_audio_transforms(num_samples=sample_rate)
+
+        spec_transform = nn.Sequential(
+            torchaudio.transforms.MelSpectrogram(
+                sample_rate=sample_rate,
+                n_fft=n_fft,
+                n_mels=n_mels,
+            ),
+            torchaudio.transforms.AmplitudeToDB(stype=stype, top_db=top_db),
+        )
+
+        audio = transform(audio)
+        audio = spec_transform(audio)
+        assert audio.shape[1] == 128
+        assert audio.shape[2] == 44
+
